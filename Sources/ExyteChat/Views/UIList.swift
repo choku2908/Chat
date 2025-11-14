3405735b09674d7b46f8feb7e9ae8609f042e978
choku2908 <choku2908@gmail.com>
2025-11-14 17:17:15 +0900

    Update tableContentHeight after reloadData

--- Sources/ExyteChat/Views/UIList.swift
+++ Sources/ExyteChat/Views/UIList.swift
@@ -132,6 +132,12 @@ struct UIList<MessageContent: View, InputView: View>: UIViewRepresentable {
 //        }
         tableView.reloadData()
         coordinator.sections = sections
+        
+        if isScrolledToBottom || isScrolledToTop {
+            if !isScrollEnabled {
+                tableContentHeight = tableView.contentSize.height
+            }
+        }
     }
 
     nonisolated private func performSplitInBackground(_  prevSections:  [MessagesSection], _ sections: [MessagesSection]) async -> SplitInfo {
